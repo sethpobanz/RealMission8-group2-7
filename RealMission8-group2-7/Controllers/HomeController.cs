59666ea21af0562faa03308f4d623088cb669a14
sethpobanz <sethpobanz@gmail.com>
2024-02-28 16:04:03 -0700

    Working database

--- RealMission8-group2-7/Controllers/HomeController.cs
+++ RealMission8-group2-7/Controllers/HomeController.cs
@@ -18,10 +18,7 @@ namespace RealMission8_group2_7.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
